09a30e08a6c9d359188f3ae88a0cfaf93df9eb10
S <q20142015@yandex.ru>
2023-03-27 13:50:49 +0300

    Почти тоже

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,18 @@ public class Main {
         container.addCount(5672);
         System.out.println(container.getCount());
 
-        char[] rusalf=new char[]{'А','а','Б','б','В','в','Г','г','Д','д','Е','е','Ё','ё','Ж','ж','З','з','И','и','Й','й','К','к','Л','л','М','м','Н','н','О','о','П','п','Р','р','С','с','Т','т','У','у','Ф','ф','Х','х','Ц','ц','Ч','ч','Ш','ш','Щ','щ','ъ','Ы','ы','ь','Э','э','Ю','ю','Я','я'};
-        int razmerAlf=rusalf.length;
-        int[] rusalfcode=new int[razmerAlf];
-        for (int j=0;j<razmerAlf;j++) {
+        char[] rusalf = new char[]{'А', 'а', 'Б', 'б', 'В', 'в', 'Г', 'г', 'Д', 'д', 'Е', 'е', 'Ё', 'ё', 'Ж', 'ж', 'З', 'з', 'И', 'и', 'Й', 'й', 'К', 'к', 'Л', 'л', 'М', 'м', 'Н', 'н', 'О', 'о', 'П', 'п', 'Р', 'р', 'С', 'с', 'Т', 'т', 'У', 'у', 'Ф', 'ф', 'Х', 'х', 'Ц', 'ц', 'Ч', 'ч', 'Ш', 'ш', 'Щ', 'щ', 'Ъ', 'ъ', 'Ы', 'ы', 'Ь', 'ь', 'Э', 'э', 'Ю', 'ю', 'Я', 'я'};
+        int razmerAlf = rusalf.length;
+        int[] rusalfcode = new int[razmerAlf];
+        for (int j = 0; j < razmerAlf; j++) {
             for (int i = 0; i <= 65535; i++) {
-                if (rusalf[j]==(char)i)
-            {
-                rusalfcode[j]=i;
-                break;
-            }
+                if (rusalf[j] == (char) i) {
+                    rusalfcode[j] = i;
+                    break;
+                }
             }
         }
-        System.out.println( rusalfcode[63]);
+
+        System.out.println(rusalfcode[razmerAlf - 1]);
     }
 }
